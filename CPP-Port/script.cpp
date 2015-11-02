6e65a6b3a763003625ebc9322ba8f3d0d8daf1c1
kopalov <backup_kpa@mail.ru>
2015-11-02 17:19:58 +0300

    Update script.cpp

--- CPP-Port/script.cpp
+++ CPP-Port/script.cpp
@@ -1,1 +1,21 @@
+#include "script.h"
 
+void update()
+{
+	Player player = PLAYER::PLAYER_ID();
+	Ped playerPed = PLAYER::PLAYER_PED_ID();
+	
+	
+	
+	
+}
+
+void main()
+{
+  
+}
+
+void ScriptMain()
+{
+  
+}
