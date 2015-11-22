51b3c771a80ee08ded01aed920c4d4d65b17478a
kopalov <backup_kpa@mail.ru>
2015-11-22 04:51:12 +0300

    created C# branch and base for C# conversion

---
+++ C# Branch/Base_Module/Base_module/BasicFunctions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using GTA;
+using GTA.Math;
+using GTA.Native;
+
+namespace BaseModule
+{
+   public class bFunctions : Script
+   {
+      private bool _isModuleOn;
+      
+      public void 
+      {
+
+      }
+
+      internal void
+      {
+        
+      }
+      
+      protected override void 
+      {
+         
+      }
+
+      private static void
+      {
+         
+      }
+
+     
+   }
+}
