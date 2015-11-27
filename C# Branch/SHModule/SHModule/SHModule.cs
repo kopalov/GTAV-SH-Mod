3253126945563ed1caa64c6bf26fcf06852de103
kopalov <backup_kpa@mail.ru>
2015-11-27 12:11:43 +0300

    Structure changed (last time hopefully)

--- C# Branch/SHModule/SHModule/SHModule.cs
+++ C# Branch/SHModule/SHModule/SHModule.cs
@@ -4,14 +4,14 @@ using GTA.Math;
 using GTA.Native;
 using System.Windows.Forms;
 
-namespace SH_Module
+namespace SHModule
 {
-    public class SH_Module : Script
+    public class SHModule : Script
     {
         public double[,] house = new double[2, 3] { { 2200.8125, 3318.0876464844, 46.945693969727 }, { -952.35943603516, -1077.5021972656, 2.6772258281708 } };
 
 
-        public SH_Module()
+        public SHModule()
         {
             Tick += OnTick;
         }
---
+++ C# Branch/SHModule/SHModule/functions/Proximity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GTA;
+using GTA.Math;
+using GTA.Native;
+using System.Windows.Forms;
+using static SHModule.Variables;
+
+namespace SHModule
+{
+    public class Proximity : Script
+    {
+        public void OutsideProximity()
+        {
+            int a = 0;
+            double[] array = new double house[a];
+            while (a<array.Length)
+            {
+
+            }
+
+        }
+    }
+}
